27e1b38810841321a36c0344a0d42229c4c08686
Jessie Odeh <jessieodeh08@gmail.com>
2024-03-14 17:27:41 +0100

    style: adjust description height on mobile screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
  return (
   <main class='dark min-h-screen'>
-   <div class='mx-auto h-48 w-full bg-[var(--bg-tertiary)] p-4 sm:mt-4 sm:w-[90%] sm:rounded-xl lg:w-[600px]'>
+   <div class='mx-auto h-fit w-full bg-[var(--bg-tertiary)] p-4 sm:mt-4 sm:w-[90%] sm:rounded-xl lg:w-[600px]'>
     <h1 class='font-bold'>RoPasses</h1>
     <br />
-    <p>RoPasses is a simple website that enables retrieving very minimal extra insights on a roblox player's owned gamepasses.</p>
+    <p>RoPasses is a simple website that enables retrieving very minimal extra insights on a roblox user's owned gamepasses.</p>
     <div class='text-right text-sm hover:underline'>
      Download the extension
      <br />
